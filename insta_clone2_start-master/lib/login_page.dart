93ef650d6f1d19f20f1cf985b98d8719ef8dd3b9
qf9ar8nv <ckguscjf0@gmail.com>
2021-02-24 22:42:16 +0900

    make instagram_demo_app
    * flutter를 이용한 서비스 제작 공부.
    * flutter와 firebase를 이용한 사용자 구글 로그인 서비스 구현 중.

---
+++ insta_clone2_start-master/lib/login_page.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:chapter10/root_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'tab_page.dart';
+
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram Clone',
+              style: GoogleFonts.pacifico(
+                fontSize: 40.0,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(50.0),
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {
+                _handleSignIn();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+    // 구글 로그인을 수행하고 firebaseuser를 반환.
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser
+    .authentication;
+    //구글 로그인으로 인증된 정보를 기반으로 firebaseuser객체를 구성
+    FirebaseUser user = (await _auth.signInWithCredential(
+        GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken))).user;
+  }
+}
